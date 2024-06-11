b4561a270397664b46a6ed59632434f0e1f9ded5
Filip Kawulok <125468679+az-fkaw@users.noreply.github.com>
2024-06-11 08:07:53 +0200

    Coding page switching logic

--- webpage_source/homePage.py
+++ webpage_source/homePage.py
@@ -5,3 +5,23 @@ st.set_page_config(page_title='Domovská stránka')
 
 st.header('Hello dear listener!')
 st.write('Please select up to 25 songs you want us to play from these categories:')
+
+col1, col2 = st.columns(2, gap="small")
+
+with col1:
+    if st.button("Duety"):
+        st.switch_page("pages/Duety.py")
+with col1:
+    if st.button("Rock and Roll"):
+        st.switch_page("pages/Rock and Roll.py")
+with col1:
+    if st.button("Zpěvačky"):
+        st.switch_page("pages/Zpěvačky.py")
+with col2:
+    if st.button("Zpěváci"):
+        st.switch_page("pages/Zpěváci.py")
+with col2:
+    if st.button("České písně"):
+        st.switch_page("pages/České písně.py")
+
+st.write('or see the full list of songs:')
