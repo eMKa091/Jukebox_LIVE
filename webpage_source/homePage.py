baaabfe924ed57753f5451688a16784939745d26
Filip Kawulok <125468679+az-fkaw@users.noreply.github.com>
2024-06-11 07:31:03 +0200

    Renaming

--- webpage_source/homePage.py
+++ webpage_source/homePage.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(page_title='Domovská stránka')
+
 st.header('Hello dear listener!')
 st.write('Please select up to 25 songs you want us to play from these categories:')
