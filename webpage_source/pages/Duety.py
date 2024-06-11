923227c7c68df6a27ee7166bc41232d1c159b69b
Filip Kawulok <125468679+az-fkaw@users.noreply.github.com>
2024-06-11 08:29:28 +0200

    Implementing test logic

--- webpage_source/pages/Duety.py
+++ webpage_source/pages/Duety.py
@@ -1,7 +1,16 @@
 import streamlit as st
 import pandas as pd
+from streamlit.report_thread import get_report_ctx
 
 st.set_page_config(page_title='Duety')
+
+def get_session_id():
+    return get_report_ctx().session_id
+
+def get_session_state():
+    session_id = get_session_id()
+    session_state = st.session_state.get(session_id=session_id, selected_songs=[], total_selected=0)
+    return session_state
 
 csvDuetyPath = "./dataSources/duety.csv"
 
@@ -10,5 +19,14 @@ def load_duets_data(path):
     df = pd.read_csv(path)
     return df
 
+session_state = get_session_state()
+
 duetsDF = load_duets_data(csvDuetyPath)
-st.dataframe(duetsDF, hide_index=True)
+for index, row in duetsDF.iterrows():
+    selected = st.checkbox(f"{row['Song']} by {row['Artists']}")
+    if selected:
+        if row['Song'] not in session_state.selected_songs:
+            session_state.selected_songs.append(row['Song'])
+            session_state.total_selected += 1
+
+st.write(f"Total selected songs: {session_state.total_selected}")
