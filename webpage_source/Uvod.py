ff0029091771759cc6ac26a941ca7b15c7310644
Filip Kawulok <125468679+az-fkaw@users.noreply.github.com>
2024-06-11 18:09:26 +0200

    Cleanup

--- webpage_source/Uvod.py
+++ webpage_source/Uvod.py
@@ -8,23 +8,3 @@ if uniqueID:
     st.session_state.uniqueID = uniqueID
     st.success("Uloženo! Přesuneme vás na další stránku...")
     st.switch_page("pages/Duety.py")
-
-#col1, col2 = st.columns(2, gap="small")
-
-#with col1:
-#    if st.button("Duety"):
-#        st.switch_page("pages/Duety.py")
-#with col1:
-#    if st.button("Rock and Roll"):
-#        st.switch_page("pages/Rock and Roll.py")
-#with col1:
-#    if st.button("Zpěvačky"):
-#        st.switch_page("pages/Zpěvačky.py")
-#with col2:
-#    if st.button("Zpěváci"):
-#        st.switch_page("pages/Zpěváci.py")
-#with col2:
-#    if st.button("České písně"):
-#        st.switch_page("pages/České písně.py")
-
-#st.write('or see the full list of songs:')
