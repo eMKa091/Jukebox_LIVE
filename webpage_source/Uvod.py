9f1935694a907a899a66dc0baf407affa0970d32
Filip Kawulok <125468679+az-fkaw@users.noreply.github.com>
2024-06-12 06:52:42 +0200

    RefinedFrontPageLogic

--- webpage_source/Uvod.py
+++ webpage_source/Uvod.py
@@ -1,17 +1,24 @@
 import streamlit as st
 from random import randint
 
-st.header('Dobrý den, vážení hosté!')
-
 # Check if the uniqueID is already in session state
 if 'uniqueID' in st.session_state:
-    st.success(f"Vítejte zpět, {st.session_state.uniqueID}!")
-    st.write("Již jste zadali své jméno a hlasovat můžete pouze jednou.")
+    st.success(f"Vítej zpět, {st.session_state.uniqueID}!")
+    st.write("Tvou přezdívku už známe - hlasovat můžeš pouze jednou.")
     
     # Add a button to navigate to the next page
-    if st.button("Pokračovat na další stránku"):
+    if st.button("Pokračovat na České písně"):
         st.switch_page("pages/1_České písně.py")
+    if st.button("Pokračovat na Duety"):
+        st.switch_page("pages/2_Duety.py")
+    if st.button("Pokračovat na Rock and roll"):
+        st.switch_page("pages/3_Rock and Roll.py")
+    if st.button("Pokračovat na mužské interprety"):
+        st.switch_page("pages/4_Zpěváci.py")
+    if st.button("Pokračovat na ženské interprety"):
+        st.switch_page("pages/5_Zpěvačky.py")
 else:
+    st.header('Dobrý den, vážení hosté!')
     st.write('Zadejte prosím vaši přezdívku')
     uniqueID = st.text_input(label="Jmeno ci prezdivka", label_visibility='hidden')
 
