71bad4ee55d50611efa0765ce600d9cea6adaa71
Filip Kawulok <125468679+az-fkaw@users.noreply.github.com>
2024-06-12 06:42:12 +0200

    MaleUpravy

--- webpage_source/Uvod.py
+++ webpage_source/Uvod.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from random import randint
 
 st.header('Dobrý den, vážení hosté!')
 st.write('Zadejte prosím vaši přezdívku')
@@ -7,4 +8,6 @@ uniqueID = st.text_input(label="Jmeno ci prezdivka", label_visibility='hidden')
 if uniqueID:
     st.session_state.uniqueID = uniqueID
     st.success("Uloženo! Přesuneme vás na další stránku...")
-    st.switch_page("pages/Duety.py")
+    randomNumber = randint(1, 100)
+    st.session_state.randomNumber = randomNumber
+    st.switch_page("pages/1_České písně.py")
--- webpage_source/pages/1_České písně.py
+++ webpage_source/pages/1_České písně.py
@@ -1,29 +1,41 @@
+# Import of needed packages and their aliases
 import streamlit as st
 import pandas as pd
 import os
-from datetime import datetime
 
+# General page configuration
 st.set_page_config(page_title='České písně')
 
-# Check if uniqueID is set
+############################
+# Check if uniqueID is set #
+############################
+# uniqueID is inherited from the main page (user input) through session state
+# Until uniqueID is not specifed, user gets nowhere
 if "uniqueID" not in st.session_state:
     st.warning("Prosím zadejte přezdívku na úvodní stránce!")
     st.stop()
 
 uniqueID = st.session_state.uniqueID
+randomizer = st.session_state.randomNumber
 
 st.info('Prosím vyberte písně (celkově maximálně 25 napříč všemi kategoriemi)')
 
-# Initialize global variables for each category
+#################################################
+# Initialize global variables for each category #
+#################################################
 if "selected_indices" not in st.session_state:
     st.session_state.selected_indices = {}
 
 if "Ceske" not in st.session_state.selected_indices:
     st.session_state.selected_indices["Ceske"] = []
 
-# Load data
+############################
+#   Source data handling   #
+############################
+# Data path
 csvCeskePath = "./dataSources/ceske.csv"
 
+# Cache dataframe for better performance
 @st.cache_data
 def load_ceske_data(path):
     df = pd.read_csv(path)
@@ -31,7 +43,10 @@ def load_ceske_data(path):
 
 ceskeDF = load_ceske_data(csvCeskePath)
 
-# Display checkboxes and update selected indices for České písně category
+############################
+#   User interface build   #
+############################
+# Display checkboxes and update selected indices for given category
 for index, row in ceskeDF.iterrows():
     selected = index in st.session_state.selected_indices["Ceske"]
     selected = st.checkbox(f"{row['Umelec']} - {row['Pisen']}", value=selected, key=f"checkbox_{index}")
@@ -43,7 +58,7 @@ for index, row in ceskeDF.iterrows():
 # Calculate total selected songs for all categories
 total_selected = sum(len(indices) for indices in st.session_state.selected_indices.values())
 
-# Label for the progress bar
+# Label for the progress bar below
 progress_label = f"Celkem vybráno {total_selected} z 25 písní"
 
 # Progress bar with a target of 25 total songs
@@ -51,9 +66,12 @@ progress = min(total_selected / 25, 1.0)
 st.progress(progress)
 st.text(progress_label)
 
+##############################
+# Save the selection to file #
+##############################
 if st.button("Uložit výběr z dané kategorie"):
-    # Use uniqueID to generate unique filename
-    file_name = f"selected_songs_ceske_{uniqueID}.txt"
+    # Use uniqueID and random number to generate unique filename
+    file_name = f"selected_songs_ceske_{uniqueID}-{randomizer}.txt"
     
     # Construct file path
     file_path = os.path.join("webpage_source", "vote_results", file_name)
@@ -61,7 +79,7 @@ if st.button("Uložit výběr z dané kategorie"):
     # Write selected songs to the file
     with open(file_path, "w") as file:
         for index in st.session_state.selected_indices["Ceske"]:
-            file.write(f"{ceskeDF.iloc[index]['Pisen']} od {ceskeDF.iloc[index]['Umelec']}\n")
+            file.write(f"{ceskeDF.iloc[index]['Umelec']} - {ceskeDF.iloc[index]['Pisen']}\n")
     
     # Show success message
     st.success("Výběr písní z dané kategorie byl uložen!")
