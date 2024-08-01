e0d08686436655f1de3e1112e32df4e417d3bdfa
Filip Kawulok <125468679+az-fkaw@users.noreply.github.com>
2024-08-01 20:17:24 +0200

    Revert "Done"
    This reverts commit 0715673e211f21e66c2852ab6cb253601fb7c654.

--- webpage_source/pages/1_České písně.py
+++ webpage_source/pages/1_České písně.py
@@ -1,18 +1,16 @@
-# Import needed packages and their aliases
+# Import of needed packages and their aliases
 import streamlit as st
 import pandas as pd
-import importlib
 import os
-from streamlit_navigation_bar import st_navbar
 
 # General page configuration
-st.set_page_config(page_title="České písně", initial_sidebar_state="collapsed")
+st.set_page_config(page_title='České písně')
 
 ############################
 # Check if uniqueID is set #
 ############################
 # uniqueID is inherited from the main page (user input) through session state
-# Until uniqueID is not specified, user gets nowhere
+# Until uniqueID is not specifed, user gets nowhere
 if "uniqueID" not in st.session_state:
     st.warning("Prosím zadejte přezdívku na úvodní stránce!")
     st.stop()
@@ -46,22 +44,6 @@ ceskeDF = load_ceske_data(csvCeskePath)
 ############################
 #   User interface build   #
 ############################
-# Define the navigation options
-nav_options = ["Duety", "Rock and Roll"]
-
-# Create the navigation bar
-selected_page = st_navbar(nav_options)
-st.divider()
-
-# Load the selected page content using importlib
-if selected_page == "Duety":
-    duety_module = importlib.import_module('pages.2_Duety')
-    duety_module.duety()
-elif selected_page == "Rock and Roll":
-    rock_and_roll_module = importlib.import_module('pages.3_Rock and Roll')
-    rock_and_roll_module.RockAndRoll()
-
-
 st.divider()
 st.info('Prosím vyberte písně (celkově maximálně 25 napříč všemi kategoriemi)')
 st.divider()
--- webpage_source/pages/2_Duety.py
+++ webpage_source/pages/2_Duety.py
@@ -1,107 +1,92 @@
-# Import needed packages and their aliases
+# Import of needed packages and their aliases
 import streamlit as st
 import pandas as pd
-import importlib
 import os
-from streamlit_navigation_bar import st_navbar
 
 # General page configuration
-st.set_page_config(page_title="Duety", initial_sidebar_state="collapsed")
+st.set_page_config(page_title='Duety')
 
-def duety():
-    ############################
-    # Check if uniqueID is set #
-    ############################
-    # uniqueID is inherited from the main page (user input) through session state
-    # Until uniqueID is not specifed, user gets nowhere
-    if "uniqueID" not in st.session_state:
-        st.warning("Prosím zadejte přezdívku na úvodní stránce!")
-        st.stop()
+############################
+# Check if uniqueID is set #
+############################
+# uniqueID is inherited from the main page (user input) through session state
+# Until uniqueID is not specifed, user gets nowhere
+if "uniqueID" not in st.session_state:
+    st.warning("Prosím zadejte přezdívku na úvodní stránce!")
+    st.stop()
 
-    uniqueID = st.session_state.uniqueID
-    randomizer = st.session_state.randomNumber
+uniqueID = st.session_state.uniqueID
+randomizer = st.session_state.randomNumber
 
-    #################################################
-    # Initialize global variables for each category #
-    #################################################
-    if "selected_indices" not in st.session_state:
-        st.session_state.selected_indices = {}
+#################################################
+# Initialize global variables for each category #
+#################################################
+if "selected_indices" not in st.session_state:
+    st.session_state.selected_indices = {}
 
-    if "Duety" not in st.session_state.selected_indices:
-        st.session_state.selected_indices["Duety"] = []
+if "Duety" not in st.session_state.selected_indices:
+    st.session_state.selected_indices["Duety"] = []
 
-    ############################
-    #   Source data handling   #
-    ############################
-    # Data path
-    csvDuetyPath = "./dataSources/duety.csv"
+############################
+#   Source data handling   #
+############################
+# Data path
+csvDuetyPath = "./dataSources/duety.csv"
 
-    @st.cache_data
-    def load_duets_data(path):
-        df = pd.read_csv(path)
-        return df
+@st.cache_data
+def load_duets_data(path):
+    df = pd.read_csv(path)
+    return df
 
-    duetsDF = load_duets_data(csvDuetyPath)
+duetsDF = load_duets_data(csvDuetyPath)
 
-    ############################
-    #   User interface build   #
-    ############################
-    # Define the navigation options
-    nav_options = ["České písně", "Rock and Roll"]
+############################
+#   User interface build   #
+############################
+############################
+# Dropdown navigation menu #
+############################
+st.divider()
+st.info('Prosím vyberte písně (celkově maximálně 25 napříč všemi kategoriemi)')
+st.divider()
 
-    # Create the navigation bar
-    selected_page = st_navbar(nav_options)
-    st.divider()
+# Calculate total initially selected songs for all categories
+total_selected = sum(len(indices) for indices in st.session_state.selected_indices.values())
 
-    # Load the selected page content using importlib
-    if selected_page == "České písně":
-        duety_module = importlib.import_module('pages.1_České písně')
-        duety_module.ceskePisne()
-    elif selected_page == "Rock and Roll":
-        rock_and_roll_module = importlib.import_module('pages.3_Rock and Roll')
-        rock_and_roll_module.RockAndRoll()
+# Display checkboxes and update selected indices for given category
+for index, row in duetsDF.iterrows():
+    selected = index in st.session_state.selected_indices["Duety"]
+    disabled = False
+    if total_selected >= 25 and not selected:
+        disabled = True
+    selected = st.checkbox(f"{row['Umelec']} - {row['Pisen']}", value=selected, disabled=disabled, key=f"checkbox_{index}")
+    if selected and index not in st.session_state.selected_indices["Duety"]:
+        st.session_state.selected_indices["Duety"].append(index)
+        total_selected += 1  # Increment total selected count
+    elif not selected and index in st.session_state.selected_indices["Duety"]:
+        st.session_state.selected_indices["Duety"].remove(index)
+        total_selected -= 1  # Decrement total selected count
 
-    st.divider()
-    st.info('Prosím vyberte písně (celkově maximálně 25 napříč všemi kategoriemi)')
-    st.divider()
+# Label for the progress bar
+progress_label = f"Celkem vybráno {total_selected} z 25 písní"
 
-    # Calculate total initially selected songs for all categories
-    total_selected = sum(len(indices) for indices in st.session_state.selected_indices.values())
+# Progress bar with a target of 25 total songs
+progress = min(total_selected / 25, 1.0)
+st.progress(progress)
+st.text(progress_label)
 
-    # Display checkboxes and update selected indices for given category
-    for index, row in duetsDF.iterrows():
-        selected = index in st.session_state.selected_indices["Duety"]
-        disabled = False
-        if total_selected >= 25 and not selected:
-            disabled = True
-        selected = st.checkbox(f"{row['Umelec']} - {row['Pisen']}", value=selected, disabled=disabled, key=f"checkbox_{index}")
-        if selected and index not in st.session_state.selected_indices["Duety"]:
-            st.session_state.selected_indices["Duety"].append(index)
-            total_selected += 1  # Increment total selected count
-        elif not selected and index in st.session_state.selected_indices["Duety"]:
-            st.session_state.selected_indices["Duety"].remove(index)
-            total_selected -= 1  # Decrement total selected count
-
-    # Label for the progress bar
-    progress_label = f"Celkem vybráno {total_selected} z 25 písní"
-
-    # Progress bar with a target of 25 total songs
-    progress = min(total_selected / 25, 1.0)
-    st.progress(progress)
-    st.text(progress_label)
-
-    # Button to send selected songs list
-    if st.button("Uložit výběr z dané kategorie"):
-        # Use uniqueID and random number to generate unique filename
-        file_name = f"selected_songs_duety_{uniqueID}-{randomizer}.txt"
-        
-        # Construct file path
-        file_path = os.path.join("webpage_source", "vote_results", file_name)
-        
-        # Write selected songs to the file
-        with open(file_path, "w") as file:
-            for index in st.session_state.selected_indices["Duety"]:
-                file.write(f"{duetsDF.iloc[index]['Pisen']} od {duetsDF.iloc[index]['Umelec']}\n")
-        
-        # Show success message
-        st.success("Výběr písní z dané kategorie byl uložen!")
+# Button to send selected songs list
+if st.button("Uložit výběr z dané kategorie"):
+    # Use uniqueID and random number to generate unique filename
+    file_name = f"selected_songs_duety_{uniqueID}-{randomizer}.txt"
+    
+    # Construct file path
+    file_path = os.path.join("webpage_source", "vote_results", file_name)
+    
+    # Write selected songs to the file
+    with open(file_path, "w") as file:
+        for index in st.session_state.selected_indices["Duety"]:
+            file.write(f"{duetsDF.iloc[index]['Pisen']} od {duetsDF.iloc[index]['Umelec']}\n")
+    
+    # Show success message
+    st.success("Výběr písní z dané kategorie byl uložen!")
