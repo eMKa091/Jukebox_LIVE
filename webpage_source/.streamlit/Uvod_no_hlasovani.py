12174a40747a7563a38d0149d42afe956ece2e62
Filip Kawulok <125468679+az-fkaw@users.noreply.github.com>
2024-08-25 13:04:03 +0200

    SplashScreenOut

---
+++ webpage_source/.streamlit/Uvod_no_hlasovani.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.header("Vážení hosté,")
+st.header("aktuálně neprobíhá žádné hlasování...")
--- webpage_source/Uvod.py
+++ webpage_source/Uvod.py
@@ -1,4 +1,25 @@
 import streamlit as st
+from random import randint
 
-st.header("Vážení hosté,")
-st.header("aktuálně neprobíhá žádné hlasování...")
+###################################################
+# Show welcome back page if user is known already #
+###################################################
+# Check if the uniqueID is already in session state
+if 'uniqueID' in st.session_state:
+    st.success(f"Vítej zpět, {st.session_state.uniqueID}!")
+    st.write("Tvou přezdívku už známe - hlasovat můžeš pouze jednou.")
+    
+###############################################
+# Show initial screen if the session is fresh #
+###############################################
+else:
+    st.header('Dobrý den, vážený hoste!')
+    st.write('Zadej prosím svou přezdívku')
+    uniqueID = st.text_input(label="Jmeno ci prezdivka", label_visibility='hidden')
+
+    if uniqueID:
+        st.session_state.uniqueID = uniqueID
+        st.success("Uloženo! Přesuneme vás na další stránku...")
+        randomNumber = randint(1, 100)
+        st.session_state.randomNumber = randomNumber
+        st.switch_page("pages/1_České písně.py")
