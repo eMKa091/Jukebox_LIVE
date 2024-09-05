06b351a2f57a43dd409adab5698d38196febd299
Filip Kawulok <125468679+az-fkaw@users.noreply.github.com>
2024-09-05 13:28:13 +0200

    Update Uvod.py

--- webpage_source/Uvod.py
+++ webpage_source/Uvod.py
@@ -15,7 +15,7 @@ def admin_page():
 
     # Option to log out (removes admin query parameter)
     if st.button("Log Out"):
-        st.query_params.setdefault()   # Clear query params
+        st.query_params.clear   # Clear query params
         st.success("Logged out of admin view. Refresh to return to the normal view.")
 
 # Function to show the main page for regular users
@@ -43,7 +43,7 @@ def main_page():
             st.session_state.uniqueID = uniqueID
             st.session_state.randomNumber = randint(1, 100)
             st.success("Uloženo! Přesuneme vás na hlasování...")
-            st.query_params.setdefault()  # Clear any query params before switching pages
+            st.query_params.clear  # Clear any query params before switching pages
             st.switch_page("pages/Hlasování.py")  # Make sure the file name matches exactly
 
 # Get the query parameters using the new API
