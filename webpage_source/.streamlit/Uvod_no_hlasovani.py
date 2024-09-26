9f702526a6a1d26bc8b7c76055ce86229eae527b
Filip Kawulok <125468679+az-fkaw@users.noreply.github.com>
2024-09-26 22:25:56 +0200

    Splash

--- webpage_source/.streamlit/Uvod_no_hlasovani.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.header("Vážení hosté,")
-st.header("Prosím zadejte přezdívku na úvodní stránce!...")
---
+++ webpage_source/Uvod_no_hlasovani.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+# Streamlit app configuration
+st.set_page_config(page_title="", page_icon="📞")
+
+st.header("Vážení hosté,")
+st.header("Aktuálně neprobíhá žádné hlasvání.")
+
+# Displaying contact information
+st.subheader("📞 Phone")
+st.write("+420 608 462 008")
+
+st.subheader("✉️ Email")
+st.write("[rudyhorvat77@gmail.com](mailto:rudyhorvat77@gmail.com)")
+
+# Divider for styling purposes
+st.divider()
