e74441e302dcfef88fc26545b255ecc18013dcb0
Filip Kawulok <125468679+az-fkaw@users.noreply.github.com>
2024-09-26 22:32:24 +0200

    uu

--- webpage_source/Uvod.py
+++ webpage_source/Uvod.py
@@ -4,14 +4,13 @@ import streamlit as st
 st.set_page_config(page_title="", page_icon="📞")
 
 st.header("Vážení hosté,")
-st.header("Aktuálně neprobíhá žádné hlasvání.")
+st.header("Aktuálně neprobíhá žádné hlasování.")
+
+st.subheader("Neváhejte nás kontaktovat:")
 
 # Displaying contact information
-st.subheader("📞 Phone")
-st.write("+420 608 462 008")
-
-st.subheader("✉️ Email")
-st.write("[rudyhorvat77@gmail.com](mailto:rudyhorvat77@gmail.com)")
+st.write("📞 +420 608 462 008")
+st.write("✉️ [rudyhorvat77@gmail.com](mailto:rudyhorvat77@gmail.com)")
 
 # Divider for styling purposes
 st.divider()
