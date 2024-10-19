822adb58b9be83d5f3f7b8320fa61b45fcee6c9b
Filip Kawulok <125468679+az-fkaw@users.noreply.github.com>
2024-10-19 06:43:40 +0200

    Vseok

--- jukeboxHeroes-v2/index.py
+++ jukeboxHeroes-v2/index.py
@@ -3,7 +3,7 @@ from admin import admin_page
 from voting import voting_page
 from database import add_admin_user
 
-add_admin_user("a", "a")
+#add_admin_user("a", "a")
 
 if 'page' not in st.session_state:
     st.session_state['page'] = 'admin'  # Default to admin page
