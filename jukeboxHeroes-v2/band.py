20ff445a4b0597e5fa497574d431e3ea20b09de3
Filip Kawulok <125468679+az-fkaw@users.noreply.github.com>
2024-10-31 12:12:26 +0100

    Bold

--- jukeboxHeroes-v2/band.py
+++ jukeboxHeroes-v2/band.py
@@ -6,12 +6,19 @@ def band_page():
 
     # Retrieve the current playlist content from the database
     playlist_content = get_band_page_content()
-    
+
     if playlist_content:
         # Replace newline characters with <br> for line breaks
         playlist_content = playlist_content.replace("\n", "<br>")
         # Replace tab characters with four non-breaking spaces for indentation
         playlist_content = playlist_content.replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;")
-        st.markdown(playlist_content, unsafe_allow_html=True)
+        
+        # Wrap the content in a <div> with custom style for larger, bold text
+        styled_content = f"""
+        <div style="font-size:20px; font-weight:bold; line-height:1.5;">
+            {playlist_content}
+        </div>
+        """
+        st.markdown(styled_content, unsafe_allow_html=True)
     else:
         st.info("No playlist content defined yet.")
