c21f56b937173e91ce3f34bf87878362870e940e
Filip Kawulok <125468679+az-fkaw@users.noreply.github.com>
2025-03-17 19:16:07 +0100

    Update gh_utils.py

--- jukeboxHeroes-v2/gh_utils.py
+++ jukeboxHeroes-v2/gh_utils.py
@@ -1,31 +1,13 @@
 import os 
 import requests
 import base64
-import sqlite3
 
 BACKUP_DIR = 'backups/'
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 GITHUB_USERNAME = 'az-fkaw'
 GITHUB_REPO = 'Jukebox_LIVE'
 BACKUP_FOLDER = 'jukeboxHeroes-v2/backups'
-DB_PATH = 'votes.db'
-
-def check_db_validity():
-    if not os.path.exists(DB_PATH):
-        print("Database does not exist.")
-        return False
-    
-    # Check if the events table exists
-    try:
-        conn = sqlite3.connect(DB_PATH)
-        cursor = conn.cursor()
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='events';")
-        table_exists = cursor.fetchone() is not None
-        conn.close()
-        return table_exists
-    except sqlite3.Error as e:
-        print(f"Error checking database: {e}")
-        return False
+DB_PATH = './votes.db'
 
 def download_database_from_github():
     # URL to access the GitHub content
@@ -47,12 +29,6 @@ def download_database_from_github():
         with open(DB_PATH, 'wb') as f:
             f.write(base64.b64decode(cleaned_content))
         print(f"Database downloaded from GitHub: {DB_PATH}")
-        
-        # Check if the file is valid
-        if not check_db_validity():
-            print("Error: Database is empty or corrupted.")
-            return
-        
-        print(f"Database is valid and ready.")
+
     else:
         print(f"Failed to download database from GitHub: {response.status_code}")
